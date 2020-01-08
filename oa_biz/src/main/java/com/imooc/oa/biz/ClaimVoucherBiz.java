418f011a0c4922a4df9d91be32bbd7b585e234e1
maoshiqiang <maoshiqiang@jd.com>
2020-01-08 15:47:16 +0800

    2020.0108 commit

---
+++ oa_biz/src/main/java/com/imooc/oa/biz/ClaimVoucherBiz.java
@@ -0,0 +1,24 @@
+package com.imooc.oa.biz;
+
+import com.imooc.oa.entity.ClaimVoucher;
+import com.imooc.oa.entity.ClaimVoucherItem;
+import com.imooc.oa.entity.DealRecord;
+
+import java.util.List;
+
+//声明业务处理的接口
+public interface ClaimVoucherBiz {
+
+    void save(ClaimVoucher claimVoucher, List<ClaimVoucherItem> items);//保存报销单
+
+    ClaimVoucher get(int id);//获取报销单编号
+    List<ClaimVoucherItem> getItems(int cvid);//获取报销单条目
+    List<DealRecord> getRecords(int cvid);//获取报销单审核记录
+
+    List<ClaimVoucher> getForSelf(String sn);
+    List<ClaimVoucher> getForDeal(String sn);
+
+    void update(ClaimVoucher claimVoucher, List<ClaimVoucherItem> items);
+    void submit(int id);
+    void deal(DealRecord dealRecord);
+}
